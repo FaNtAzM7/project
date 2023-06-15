3a4ff0d53e52383025675860b81ab5ddbf1abed6
FaNtAzM <aleksey.rsvetkov7@mail.rugit config --global user.email aleksey.rsvetkov7@mail.rugit config --global user.email aleksey.rsvetkov7@mail.ru>
2023-06-15 23:03:26 +0300

    инвертирует строку и сравнивает её с предыдущей

--- 1506.py
+++
@@ -1,9 +0,0 @@
-def ispalindrom(s):
-    if s == s[::-1]:
-        return True
-    else:
-        return False
-print(ispalindrom("лепсспел"))#выведет True
-print(ispalindrom("helloworld"))#выведет False
-
-
